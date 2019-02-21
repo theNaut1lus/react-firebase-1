975b188230925f0adfd577879a6169afde2141fa
Sidak Singh Aulakh <sidakaulakh@gmail.com>
2019-02-21 14:51:02 +0530

    Added production build

---
+++ build/precache-manifest.b31d574ec967f4c61934be9d0835ab79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "dc08c73a07ccc6dbb81a",
+    "url": "/static/js/main.dc08c73a.chunk.js"
+  },
+  {
+    "revision": "c0dab5c2094154316e89",
+    "url": "/static/js/2.c0dab5c2.chunk.js"
+  },
+  {
+    "revision": "dc08c73a07ccc6dbb81a",
+    "url": "/static/css/main.fe5e01af.chunk.css"
+  },
+  {
+    "revision": "c0dab5c2094154316e89",
+    "url": "/static/css/2.5ce32d68.chunk.css"
+  },
+  {
+    "revision": "05e99f9faf282499ecb499c3c6812be5",
+    "url": "/index.html"
+  }
+];
